8b54419dcdf9e179f9621610717eab44d177d555
szorfein <szorfein@protonmail.com>
2020-05-10 20:34:55 +0200

    add widgets/tasklist

--- private_dot_config/awesome/rc.lua
+++ private_dot_config/awesome/rc.lua
@@ -119,28 +119,6 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 -- Create a textclock widget
 mytextclock = wibox.widget.textclock()
 
-local tasklist_buttons = gears.table.join(
-                     awful.button({ }, 1, function (c)
-                                              if c == client.focus then
-                                                  c.minimized = true
-                                              else
-                                                  c:emit_signal(
-                                                      "request::activate",
-                                                      "tasklist",
-                                                      {raise = true}
-                                                  )
-                                              end
-                                          end),
-                     awful.button({ }, 3, function()
-                                              awful.menu.client_list({ theme = { width = 250 } })
-                                          end),
-                     awful.button({ }, 4, function ()
-                                              awful.client.focus.byidx(1)
-                                          end),
-                     awful.button({ }, 5, function ()
-                                              awful.client.focus.byidx(-1)
-                                          end))
-
 local function set_wallpaper(s)
     -- Wallpaper
     if beautiful.wallpaper then
@@ -171,11 +149,7 @@ awful.screen.connect_for_each_screen(function(s)
     s.mytaglist = require("widgets.taglist")(s, {})
 
     -- Create a tasklist widget
-    s.mytasklist = awful.widget.tasklist {
-        screen  = s,
-        filter  = awful.widget.tasklist.filter.currenttags,
-        buttons = tasklist_buttons
-    }
+    s.mytasklist = require("widgets.tasklist")(s, {})
 
     -- Create the wibox
     s.mywibox = awful.wibar({ position = "top", height = dpi(28), screen = s })
---
+++ private_dot_config/awesome/widgets/tasklist.lua
@@ -0,0 +1,157 @@
+local awful = require("awful")
+local table = require("gears.table")
+local wibox = require("wibox")
+local beautiful = require("beautiful")
+local font = require("utils.font")
+
+local tasklist_root = class()
+
+function tasklist_root:init(args)
+  self.template = self:select_template()
+  self.buttons = self:make_buttons()
+end
+
+function tasklist_root:select_template()
+    return self:template_text() -- default
+end
+
+function tasklist_root:make_buttons()
+  local button = table.join(
+    awful.button({}, 1, function(c)
+      if c == client.focus then
+        c.minimized = true
+      else
+        c:emit_signal(
+         "request::activate",
+         "tasklist",
+         { raise = true }
+        )
+      end
+    end),
+    awful.button({}, 3, function()
+      awful.menu.client_list({ theme = { width = 250 } })
+    end),
+    awful.button({}, 4, function()
+      awful.client.focus.byidx(1)
+    end),
+    awful.button({}, 5, function()
+      awful.client.focus.byidx(-1)
+    end)
+  )
+  return button
+end
+
+local function update_icon(item, c)
+  if c.class == "Firefox" or c.class == "Brave-browser" then
+    item.text = ""
+  elseif c.name == "neomutt" then
+    item.text = ""
+  elseif c.name == "weechat" then
+    item.text = ""
+  elseif c.name:match("ncmpcpp") then
+    item.text = ""
+  elseif c.name == "vifm" then
+    item.text = ""
+  else
+    item.text = ""
+  end
+end
+
+local function update_fg(item, c)
+  if client.focus == c then
+    item.fg = M.x.primary .. "FF" -- 100%
+  elseif client.unfocus == c then
+    item.fg = M.x.primary .. "B3" -- 70%
+  elseif client.minimized == c then
+    item.fg = M.x.on_background .. "80" -- 50%
+  elseif client.urgent == c then
+    item.fg = M.x.on_error .. "FF"
+  else
+    item.fg = M.x.on_background .. "80" -- 50%
+  end
+end
+
+local function update_bg(item, c)
+  if client.focus == c then
+    item.bg = M.x.on_background .. "0D" -- 5%
+  elseif client.unfocus == c then
+    item.bg = M.x.on_background .. "0D" -- 5%
+  elseif client.minimized == c then
+    item.bg = M.x.error .. "0A" -- 4%
+  elseif client.urgent == c then
+    item.bg = M.x.error .. "FF"
+  else
+    item.bg = M.x.on_background .. "00" -- 0%
+  end
+end
+
+function tasklist_root:template_text() 
+  local t = {
+    {
+      {
+        {
+          {
+            {
+              id = 'text_icon',
+              font = M.f.icon,
+              widget = wibox.widget.textbox
+            },
+            id = "mat_fg",
+            widget = wibox.container.background
+          },
+          {
+            id = 'text_role',
+            font = M.f.button,
+            widget = wibox.widget.textbox,
+          },
+          spacing = dpi(8),
+          layout = wibox.layout.fixed.horizontal,
+        },
+        left = dpi(8), right = dpi(8),
+        widget = wibox.container.margin
+      },
+      id = 'mat_background',
+      widget = wibox.container.background,
+    },
+    widget = wibox.container.background,
+    create_callback = function(self, c, index, objects)
+
+      local icon = self:get_children_by_id('text_icon')[1]
+      update_icon(icon, c)
+
+      local bg = self:get_children_by_id('mat_background')[1]
+      update_bg(bg, c)
+
+      local fg = self:get_children_by_id('mat_fg')[1]
+      update_fg(fg, c)
+    end,
+    update_callback = function(self, c, index, objects)
+
+      local icon = self:get_children_by_id('text_icon')[1]
+      update_icon(icon, c)
+
+      local bg = self:get_children_by_id('mat_background')[1]
+      update_bg(bg, c)
+
+      local fg = self:get_children_by_id('mat_fg')[1]
+      update_fg(fg, c)
+    end,
+  }
+  return t
+end
+
+local tasklist_widget = class(tasklist_root)
+
+function tasklist_widget:init(s, args)
+  tasklist_root.init(self, args)
+
+  local w = awful.widget.tasklist {
+    screen = s,
+    filter  = awful.widget.tasklist.filter.currenttags,
+    buttons = self.buttons,
+    widget_template = self.template
+  }
+  return w
+end
+
+return tasklist_widget
