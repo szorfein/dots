fbe025ebbdd27d8e3842f7d396a66cda5d024310
szorfein <szorfein@protonmail.com>
2022-11-16 16:02:36 +0000

    picom and light in debian/stable

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -9,24 +9,16 @@ install_deps() {
   pkgs="build-essential pkg-config x11proto-core-dev libx11-dev libxft-dev \
     fontconfig x11proto-dev libxext-dev"
 
-  # picom
-  pkgs="$pkgs meson ninja-build cmake libev-dev libx11-xcb-dev libxcb-render-util0-dev \
-    libxcb-image0-dev libpixman-1-dev libxcb-damage0-dev libxcb-randr0-dev \
-    libxcb-sync-dev libxcb-composite0-dev libxcb-xinerama0-dev libxcb-present-dev \
-    libxcb-glx0-dev uthash-dev libconfig-dev libpcre3-dev libgl1-mesa-dev \
-    libdbus-1-dev"
-
   # i3lock-color
-  pkgs="$pkgs libpam0g-dev libcairo2-dev libxcb-xkb-dev libxcb-xrm-dev \
-    libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev autoconf libxcb-util0-dev"
-
-  # light
-  pkgs="$pkgs debhelper-compat"
+  pkgs="$pkgs autoconf make libpam0g-dev libcairo2-dev libfontconfig1-dev \
+    libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev \
+    libxcb-randr0-dev libxcb-image0-dev libxcb-util0-dev libxcb-xrm-dev \
+    libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev"
 
   # Ueberzug
   pkgs="$pkgs python3-pip"
 
-  sudo $ins gpg gpg-agent xclip pass zsh awesome mpd ncmpcpp xinit \
+  sudo $ins gpg gpg-agent xclip pass zsh awesome mpd ncmpcpp xinit picom light \
     xserver-xorg-core xserver-xorg-input-libinput feh scrot vifm mpv zathura fdm \
     neomutt imagemagick msmtp msmtp-mta weechat rofi youtube-dl xss-lock \
     papirus-icon-theme mpc lightdm inotify-tools stow arc-theme tmux gcc $pkgs
@@ -69,25 +61,12 @@ install_extra_deps() {
     && sudo make PREFIX=/usr DESTDIR=/ install
   )
 
-  # picom
-  PN="picom"
-  PV="8.2"
-
-  ( cd ~/builds/ \
-    && curl -L -o "$PN"-"$PV".tar.gz https://github.com/yshui/picom/archive/v"$PV".tar.gz \
-    && tar xvf "$PN"-"$PV".tar.gz \
-    && cd "$PN"-"$PV" \
-    && meson --buildtype=release . build \
-    && ninja -C build \
-    && sudo ninja -C build install
-  )
-
   # Ueberzug
   sudo pip3 install ueberzug
 
   # i3lock-color
   PN="i3lock-color"
-  PV="2.13.c.2"
+  PV="2.13.c.4"
 
   ( cd ~/builds \
     && curl -L -o "$PN"-"$PV".tar.gz https://github.com/Raymo111/"$PN"/archive/"$PV".tar.gz \
@@ -96,24 +75,6 @@ install_extra_deps() {
     && autoreconf -fiv \
     && ./configure --prefix=/usr --sysconfdir=/etc \
     && sudo make DESTDIR=/ install
-  )
-
-  # Light
-  PN="light"
-  PV="1.2.2"
-
-  # build
-  ( cd ~/builds \
-    && curl -L -o "$PN"-"$PV".tar.gz https://github.com/haikarainen/"$PN"/archive/v"$PV".tar.gz \
-    && tar xvf "$PN"-"$PV".tar.gz \
-    && cd "$PN"-"$PV" \
-    && ./autogen.sh \
-    && ./configure --with-udev && make deb
-  )
-
-  # install
-  ( cd ~/builds \
-    && sudo dpkg -i "$PN"_1.2.1-1_amd64.deb
   )
 }
 
