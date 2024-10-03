11155f061ee0405022e744e5f4dc1e361627df96
szorfein <szorfein@protonmail.com>
2024-10-03 12:54:53 +0000

    awm3 - Arch

--- home/private_dot_zsh/functions.zsh
+++ home/private_dot_zsh/functions.zsh
@@ -2,7 +2,14 @@
 
 [ -f /etc/arch-release ] && source "${HOME}/.zsh/archlinux.zsh"
 
-hello() {
-  echo "hello"
+# e.g: ssh_create_ed25519k_keys "laptop-git"
+ssh_create_ed25519_keys() {
+  [ -z "$1" ] && exit 1
+  ssh-keygen -t ed25519 -o -a 100 -f ~/.ssh/"$1"
 }
 
+# e.g: ssh_create_rsa4k_keys "laptop-git"
+ssh_create_rsa4k_keys() {
+  [ -z "$1" ] && exit 1
+  ssh-keygen -t rsa -b 4096 -o -a 100 -f ~/.ssh/"$1"
+}
