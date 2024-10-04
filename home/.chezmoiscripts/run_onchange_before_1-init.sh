25481f503297a1ffb68f615a0464ad745bf28112
szorfein <szorfein@protonmail.com>
2024-10-04 13:43:03 +0000

    Gentoo repo only when not installed

--- home/.chezmoiscripts/run_onchange_before_1-init.sh
+++ home/.chezmoiscripts/run_onchange_before_1-init.sh
@@ -11,12 +11,13 @@ AUTH=$(search_auth)
 msg "Execute $0..."
 
 if hash emerge 2>/dev/null ; then
+  repo_dir=/var/db/repos
   "$AUTH" emerge -av --changed-use app-eselect/eselect-repository
 
-  "$AUTH" eselect repository add ninjatools git https://github.com/szorfein/ninjatools.git
-  "$AUTH" eselect repository add guru git https://github.com/gentoo/guru.git
-  "$AUTH" eselect repository add brave-overlay git https://gitlab.com/jason.oliveira/brave-overlay.git
-  "$AUTH" eselect repository add librewolf git https://codeberg.org/librewolf/gentoo.git
+  [ -d "$repo_dir/ninjatools" ] || "$AUTH" eselect repository add ninjatools git https://github.com/szorfein/ninjatools.git
+  [ -d "$repo_dir/guru" ] || "$AUTH" eselect repository add guru git https://github.com/gentoo/guru.git
+  [ -d "$repo_dir/brave-overlay" ] || "$AUTH" eselect repository add brave-overlay git https://gitlab.com/jason.oliveira/brave-overlay.git
+  [ -d "$repo_dir/librewolf" ] || "$AUTH" eselect repository add librewolf git https://codeberg.org/librewolf/gentoo.git
 
   "$AUTH" emaint sync -r ninjatools
   "$AUTH" emaint sync -r guru
