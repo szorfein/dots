95d719f59ca038f366e4a5ca34c03642cf3072cd
szorfein <szorfein@protonmail.com>
2025-01-29 15:55:47 +0000

    remove few reaver files

--- home/scripts/debian/deps.sh
+++ home/scripts/debian/deps.sh
@@ -6,7 +6,7 @@ msg "Installing dependencies..."
 
 UPDT="apt-get update"
 INST="apt-get install -y"
-PKGS="pass openssh-server sshpass ansible git ruby tar unzip xz-utils"
+PKGS="pass openssh-server sshpass ansible git ruby tar unzip xz-utils stow"
 
 $AUTH $UPDT \
   && $AUTH $INST $PKGS \
