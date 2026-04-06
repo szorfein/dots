253291e009af4f3ebeef55a3d37f24efd1a69081
szorfein <szorfein@protonmail.com>
2026-04-06 12:17:01 +0000

    nvim on void, permission keys

--- home/scripts/lib.sh
+++ home/scripts/lib.sh
@@ -7,55 +7,61 @@ cyan="${tc}36m"
 white="${tc}37m"
 endc="${tc}0m"
 
-die() { echo "[-] $1"; exit 1; }
+die() {
+    echo "[-] $1"
+    exit 1
+}
 
 msg() {
-  printf "\\n${cyan}%s${endc}\\n" '--------------------------------------------------'
-  printf "${cyan}%s${white} %s${endc}\\n\\n" '-->' "$1"
+    printf "\\n${cyan}%s${endc}\\n" '--------------------------------------------------'
+    printf "${cyan}%s${white} %s${endc}\\n\\n" '-->' "$1"
 }
 
 bye() {
-  printf "\\n${cyan}%s${white} %s${endc}\\n" '-->' "End for $0"
-  printf "${cyan}%s${endc}\\n" "--------------------------------------------------"
+    printf "\\n${cyan}%s${white} %s${endc}\\n" '-->' "End for $0"
+    printf "${cyan}%s${endc}\\n" "--------------------------------------------------"
 }
 
 trap bye EXIT
 
 search_auth() {
-  if hash doas 2>/dev/null ; then
-    AUTH="doas"
-  elif hash sudo 2>/dev/null ; then
-    AUTH="sudo"
-  else
-    die "No pkg doas or sudo found, please install and configure one for $USER."
-  fi
+    if hash doas 2> /dev/null; then
+        AUTH="doas"
+    elif hash sudo 2> /dev/null; then
+        AUTH="sudo"
+    else
+        die "No pkg doas or sudo found, please install and configure one for $USER."
+    fi
 }
 
 systemd_start() {
-  if ! pgrep -x "$1" >/dev/null ; then
-    echo "Starting $1..."
-    "$AUTH" systemctl start "$1"
-  fi
+    if ! pgrep -x "$1" > /dev/null; then
+        echo "Starting $1..."
+        "$AUTH" systemctl start "$1"
+    fi
 }
 
 runit_start() {
-  if ! [ -L /var/service/sshd ] ; then
-    "$AUTH" ln -s /etc/sv/sshd /var/service/sshd
-  else
-    if ! pgrep -x "$1" >/dev/null ; then
-      "$AUTH" sv start "$1"
+    if ! [ -L /var/service/sshd ]; then
+        "$AUTH" ln -s /etc/sv/sshd /var/service/sshd
+    else
+        if ! pgrep -x "$1" > /dev/null; then
+            "$AUTH" sv start "$1"
+        fi
     fi
-  fi
 }
 
 openrc_start() {
-  if ! pgrep -x "$1" >/dev/null ; then
-    echo "Starting $1..."
-    "$AUTH" rc-service "$1" start
-  fi
+    if ! pgrep -x "$1" > /dev/null; then
+        echo "Starting $1..."
+        "$AUTH" rc-service "$1" start
+    fi
 }
 
 make_ssh_keys() {
-  [ -f "$HOME"/.ssh/ansible.pub ] \
-    || ssh-keygen -t ed25519 -o -a 100 -f "$HOME"/.ssh/ansible
+    [ -f "$HOME"/.ssh/ansible.pub ] || {
+        ssh-keygen -t ed25519 -o -a 100 -f "$HOME/.ssh/ansible"
+        chmod 0600 "$HOME/.ssh/ansible.pub"
+        chmod 0400 "$HOME/.ssh/ansible"
+    }
 }
